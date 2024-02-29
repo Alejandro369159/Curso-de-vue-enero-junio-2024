c56c2778249d7293be98f040a79a64c0e3400a55
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2024-02-29 17:04:00 -0600

    Se termina la página de lista de libros

---
+++ src/components/BookCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(["book"]);
+</script>
+
+<template>
+  <div class="book-card">
+    <img
+      src="https://th.bing.com/th/id/OIP.0MCwGzmvISrGyIc2B2xKdgHaE8?rs=1&pid=ImgDetMain"
+      alt=""
+    />
+    <div>
+      <p>Titulo: {{ book.title }}</p>
+      <p>Subtitulo: {{ book.subtitle }}</p>
+      <p>Autores: {{ book.authors }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/ExplorePage.vue
+++ src/pages/ExplorePage.vue
@@ -1,18 +1,26 @@
-<script setup></script>
+<script setup>
+import BookCard from "@/components/BookCard.vue";
+import { ref, onMounted } from "vue";
+
+const books = ref([]);
+
+async function getBooks() {
+  // Lógica de traerme los libros de la API
+  const request = await fetch("https://www.dbooks.org/api/recent");
+  // Esperar hasta la acción de arriba se complete
+  const response = await request.json();
+  books.value = response.books;
+}
+
+onMounted(getBooks);
+</script>
 
 <template>
   <section>
     <h2>Explorar Libros</h2>
     <div>
       <!-- Una sola instancia de un libro -->
-      <div>
-        <img src="" alt="" />
-        <div>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
-      </div>
+      <BookCard v-for="_book in books" :book="_book" />
     </div>
   </section>
 </template>
@@ -22,5 +30,24 @@ section {
   max-width: 800px;
   margin: auto;
   margin-top: 30px;
+}
+
+h2 {
+  text-align: center;
+}
+
+.book-card {
+  margin: 20px auto;
+  width: 80%;
+  display: flex;
+  background-color: rgb(184, 184, 184);
+}
+
+.book-card > img {
+  width: 30%;
+}
+
+.book-card > div {
+  width: 70%;
 }
 </style>
